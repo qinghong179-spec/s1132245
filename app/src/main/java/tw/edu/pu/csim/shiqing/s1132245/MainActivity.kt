bcb6356d118a7521419905c5601f849a1741b0c6
qinghong179-spec <qinghong179@gmail.com>
2025-12-01 14:35:20 +0800

    V0 洪詩晴

---
+++ app/src/main/java/tw/edu/pu/csim/shiqing/s1132245/MainActivity.kt
@@ -0,0 +1,47 @@
+package tw.edu.pu.csim.shiqing.s1132245
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import tw.edu.pu.csim.shiqing.s1132245.ui.theme.S1132245Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            S1132245Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "洪詩晴",//顯示 "Hello 洪詩晴
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    S1132245Theme {
+        Greeting("Android")
+    }
+}
